16db7b6de1608d42ed329a172c266e331fdde6a3
jgrimoldi <jgrimoldi@frba.utn.edu.ar>
2022-07-06 19:38:21 -0300

    add return button

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,16 +1,29 @@
 import { Component } from 'react';
-import { Outlet } from 'react-router-dom';
 import brandLogo from '../assets/images/logo-brand.svg';
+
+import { Button } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+const GoBack = () => {
+    const navigate = useNavigate();
+    return (
+        <div className='back-button'>
+            <Button variant='text' onClick={() => navigate(-1)} size='medium' startIcon={<ArrowBackIcon />} >Volver</Button>
+        </div>
+    )
+};
 
 export default class Home extends Component {
     render() {
         return (
             <main className='full-height bg_gradient'>
+                <GoBack />
                 <section className='text-center text-uppercase font-white'>
-                        <div className='p-3'>
-                            <img className='img-fluid mx-auto' src={brandLogo} alt="COMPANY LOGO" />
-                        </div>
-                        <Outlet />
+                    <div className='p-3'>
+                        <img className='img-fluid mx-auto' src={brandLogo} alt="COMPANY LOGO" />
+                    </div>
+                    <Outlet />
                 </section>
             </main>
         )
