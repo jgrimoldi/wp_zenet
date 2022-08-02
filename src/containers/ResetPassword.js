e5b01d041d9dbc3df2aa225aa46130f384d71a9b
jgrimoldi <jgrimoldi@frba.utn.edu.ar>
2022-08-02 00:08:37 -0300

    reset password fixed

--- src/containers/ResetPassword.js
+++ src/containers/ResetPassword.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import AuthServices from '../services/AuthServices';
 import { FormControl, InputLabel, OutlinedInput, InputAdornment, IconButton, Tooltip, ClickAwayListener } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import {
@@ -12,14 +14,13 @@ import {
   ErrorText,
   tooltipStyle
 } from '../components';
-import AuthServices from '../services/AuthServices';
 
 export const header = {
   title: 'Cambio de contraseña',
   subtitle: ''
 }
 
-export const ResetPassword = ({props}) => {
+export const ResetPassword = () => {
 
   const initialValues = { password: '', passwordVerify: '' };
   const [formValues, setFormValues] = useState(initialValues);
@@ -27,9 +28,10 @@ export const ResetPassword = ({props}) => {
   const [showPassword, setShowPassword] = useState(false);
   const [showError, setShowError] = useState({ value: '', error: null });
   const [successful, setSuccessful] = useState(null);
+  const [accessToken, setAccessToken] = useState({ email: '', token: '' });
+  const [errorToken, setErrorToken] = useState({ value: '', error: true });
 
-  console.log(props)
-
+  const { token } = useParams();
 
   const handleChange = (prop) => (event) => {
     setFormValues({ ...formValues, [prop]: event.target.value });
@@ -48,17 +50,35 @@ export const ResetPassword = ({props}) => {
   }
 
   useEffect(() => {
-    getToken();
+    if (errorToken.error) {
+      getToken();
+    }
   })
 
   const getToken = async () => {
 
     await AuthServices.resetPassword(token)
       .then(response => {
-        console.log(response)
+        const message = response.data.message;
+        const email = response.data.email;
+
+        if (message === 'PASSWORD_RESET_LINK_OK') {
+          setAccessToken({ ...accessToken, email: { email }, token: { token } });
+          setErrorToken({ ...errorToken, error: false });
+        }
+
       })
       .catch(error => {
-        console.log(error)
+        const errorMessage = error.response.data.error;
+
+        if (errorMessage === 'PASSWORD_RESET_LINK_IS_INVALID_OR_HAS_EXPIRED') {
+          setErrorToken({ ...errorToken, value: 'El link de recuperación ha expirado, vuelva a intentar', error: false });
+        } else if (errorMessage === 'ERROR_SEND_EMAIL') {
+          setErrorToken({ ...errorToken, value: 'Error al enviar el mail, vuelva a intentar', error: false });
+        } else {
+          setErrorToken({ ...errorToken, value: 'Ocurrió un error, vuelva a intentar', error: false });
+        }
+
       })
 
   }
@@ -91,20 +111,34 @@ export const ResetPassword = ({props}) => {
     }
   }
 
-  const handleSubmit = () => {
-    if (!showError.error && formValues.password === formValues.passwordVerify) {
+  const handleSubmit = async () => {
 
-      // await AuthServices.updatePassword()
-      // .then(response => {
-
-      // })
-      // .catch(error => {
-
-      // })
+    if (errorToken.error && !accessToken.email && !accessToken.token) {
 
     } else {
-      setShowError({ ...showError, value: 'Ocurrió un problema al cambiar la contraseña', error: true });
+      if (!showError.error && formValues.password === formValues.passwordVerify) {
+
+        await AuthServices.updateViaEmail(accessToken.email, formValues.password, accessToken.token)
+          .then(response => {
+            if (response.data === 'UPDATE_PASSWORD_EMAIL_OK') {
+              setSuccessful(true);
+            }
+          })
+          .catch(error => {
+            const errorMessage = error.response.data.error;
+
+            if (errorMessage === 'UPDATE_PASSWORD_EMAIL_RESET_LINK_IS_INVALID_OR_HAS_EXPIRED') {
+              setShowError({ ...showError, value: 'El link de recuperación ha expirado, vuelva a intentar', error: true });
+            } else {
+              setShowError({ ...showError, value: 'Ocurrió un error, vuelva a intentar', error: true });
+            }
+          })
+
+      } else {
+        setShowError({ ...showError, value: 'Ocurrió un problema al cambiar la contraseña', error: true });
+      }
     }
+
   }
 
   const handleMouseDownPassword = (event) => {
@@ -120,25 +154,52 @@ export const ResetPassword = ({props}) => {
             <h2 className='p-3 form-title'><strong>Recuperación de clave</strong></h2>
           </div>
           <form className='row g-3'>
+            {errorToken.error && !accessToken.email && !accessToken.token ?
+              (<>
+                < div className='col-12'>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <Tooltip sx={tooltipStyle} placement='top-end' arrow disableFocusListener disableHoverListener disableTouchListener
+                      onClose={handleClose} open={tooltip}
+                      PopperProps={{
+                        disablePortal: true,
+                      }}
+                      title='Debe contener: Mayúsculas, Minúsculas, Números y Símbolos'>
+                      <FormControl sx={inputStyle} variant='outlined' fullWidth required>
 
-            <div className='col-12'>
-              <ClickAwayListener onClickAway={handleClose}>
-                <Tooltip sx={tooltipStyle} placement='top-end' arrow disableFocusListener disableHoverListener disableTouchListener
-                  onClose={handleClose} open={tooltip}
-                  PopperProps={{
-                    disablePortal: true,
-                  }}
-                  title='Debe contener: Mayúsculas, Minúsculas, Números y Símbolos'>
+                        <InputLabel htmlFor='password'>Contraseña</InputLabel>
+                        <OutlinedInput inputProps={{ style: montserratFamily }} id='password' name='password'
+                          label='Contraseña'
+                          value={formValues.password}
+                          onBlur={validate}
+                          onKeyUp={validate}
+                          onChange={handleChange('password')}
+                          placeholder='Contraseña (8 a 64 carácteres)'
+                          type={showPassword ? 'text' : 'password'}
+                          autoComplete='new-password'
+                          endAdornment={
+                            <InputAdornment position='end'>
+                              <IconButton sx={visibilityStyle} aria-label='Mostrar/Ocultar Contraseña'
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge='end' >{showPassword ? <VisibilityOff /> : <Visibility />} </IconButton>
+                            </InputAdornment>
+                          } />
+
+                      </FormControl>
+                    </Tooltip>
+                  </ClickAwayListener>
+                </div>
+
+                <div className='col-12'>
                   <FormControl sx={inputStyle} variant='outlined' fullWidth required>
-
-                    <InputLabel htmlFor='password'>Contraseña</InputLabel>
-                    <OutlinedInput inputProps={{ style: montserratFamily }} id='password' name='password'
-                      label='Contraseña'
-                      value={formValues.password}
+                    <InputLabel htmlFor='passwordVerify'>Confirmar contraseña</InputLabel>
+                    <OutlinedInput inputProps={{ style: montserratFamily }} id='passwordVerify' name='passwordVerify'
+                      label='Confirmar contraseña'
+                      value={formValues.passwordVerify}
                       onBlur={validate}
                       onKeyUp={validate}
-                      onChange={handleChange('password')}
-                      placeholder='Contraseña (8 a 64 carácteres)'
+                      onChange={handleChange('passwordVerify')}
+                      placeholder='Confirmar contraseña'
                       type={showPassword ? 'text' : 'password'}
                       autoComplete='new-password'
                       endAdornment={
@@ -149,52 +210,33 @@ export const ResetPassword = ({props}) => {
                             edge='end' >{showPassword ? <VisibilityOff /> : <Visibility />} </IconButton>
                         </InputAdornment>
                       } />
-
                   </FormControl>
-                </Tooltip>
-              </ClickAwayListener>
-            </div>
-
-            <div className='col-12'>
-              <FormControl sx={inputStyle} variant='outlined' fullWidth required>
-                <InputLabel htmlFor='passwordVerify'>Confirmar contraseña</InputLabel>
-                <OutlinedInput inputProps={{ style: montserratFamily }} id='passwordVerify' name='passwordVerify'
-                  label='Confirmar contraseña'
-                  value={formValues.passwordVerify}
-                  onBlur={validate}
-                  onKeyUp={validate}
-                  onChange={handleChange('passwordVerify')}
-                  placeholder='Confirmar contraseña'
-                  type={showPassword ? 'text' : 'password'}
-                  autoComplete='new-password'
-                  endAdornment={
-                    <InputAdornment position='end'>
-                      <IconButton sx={visibilityStyle} aria-label='Mostrar/Ocultar Contraseña'
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        edge='end' >{showPassword ? <VisibilityOff /> : <Visibility />} </IconButton>
-                    </InputAdornment>
-                  } />
-              </FormControl>
-            </div>
-            {showError.error &&
-              <div className='col-12'>
-                <ErrorText error={showError.value} />
-              </div>
-            }
-            {successful &&
-              <div className='col-12'>
-                <div className="successful-notice">
-                  La contraseña fue reestablecida con exito!
                 </div>
-              </div>
+                {showError.error &&
+                  <div className='col-12'>
+                    <ErrorText error={showError.value} />
+                  </div>
+                }
+                {successful &&
+                  <div className='col-12'>
+                    <div className="successful-notice">
+                      La contraseña fue reestablecida con exito!
+                    </div>
+                  </div>
+                }
+                <div className='col-12'>
+                  <SubmitButton actionForm={() => handleSubmit()} buttonStyle={buttonStyle} buttonText='Confirmar' />
+                </div>
+              </>)
+              :
+              (<div className='header-form'>
+                <h2 className='p-3 form-title'><strong>{errorToken.value}</strong></h2>
+              </div>)
             }
-            <div className='col-12'>
-              <SubmitButton actionForm={() => handleSubmit()} buttonStyle={buttonStyle} buttonText='Confirmar' />
-            </div>
+
           </form>
         </div>
-      </div>
+      </div >
     </>
   )
 }
